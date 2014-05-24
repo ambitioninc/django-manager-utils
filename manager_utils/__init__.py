fa999c55248142182f8cb211cf3b36cf4586389a
Wes Kendall <wesleykendall@gmail.com>
2014-05-24 11:36:38 -0400

    Update __init__.py

--- manager_utils/__init__.py
+++ manager_utils/__init__.py
@@ -2,5 +2,5 @@
 from .version import __version__
 from .manager_utils import (
     ManagerUtilsMixin, ManagerUtilsManager, ManagerUtilsQuerySet, post_bulk_operation,
-    upsert, bulk_update, single, get_or_none, bulk_upsert, id_dict
+    upsert, bulk_update, single, get_or_none, bulk_upsert, id_dict, sync
 )
