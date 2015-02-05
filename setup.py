c7f8a19b23b068849bb09c3132431248f6bb9bf3
Wes Kendall <wes.kendall@tryambition.com>
2015-02-05 18:10:23 -0500

    updated querybuilder version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     ],
     install_requires=[
         'django>=1.6',
-        'django-query-builder>=0.5.6',
+        'django-query-builder>=0.6.0',
     ],
     tests_require=[
         'coverage',
