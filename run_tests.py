e852747186cc5998c71cc7d36ce4d023023698e2
Wes Kendall <wes.kendall@ambition.com>
2015-05-02 11:20:22 -0400

    updated run tests

--- run_tests.py
+++ run_tests.py
@@ -17,10 +17,6 @@ from django_nose import NoseTestSuiteRunner
 
 
 def run_tests(*test_args, **kwargs):
-    if 'south' in settings.INSTALLED_APPS:
-        from south.management.commands import patch_for_test_db_setup
-        patch_for_test_db_setup()
-
     if not test_args:
         test_args = ['manager_utils']
 
